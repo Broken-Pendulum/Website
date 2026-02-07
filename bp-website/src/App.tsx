629f2e34dd30797472d82f751a0bd2cfdd1c1b0d
verycoolgithubaccount <plutonium221b@gmail.com>
2026-02-07 00:28:06 -0700

    awesome parallax

--- bp-website/src/App.tsx
+++ bp-website/src/App.tsx
@@ -1,26 +1,17 @@
 ﻿import '@/index.css'
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
-import Navbar from "@/components/Navbar"
-import Footer from "@/components/Footer";
-
 import Home from "@/pages/Home";
 import Team from "@/pages/Team";
 
 function App() {
     return (
         <Router>
-            <div className="min-h-screen h-fit flex flex-col font-main text-2xl">
-                <Navbar />
-                    <Routes>
-                        <Route path="/" element={<Home />} />
-                        <Route path="/team" element={<Team />} />
-
-
-                        <Route path="*" element={<Home />} />
-                    </Routes>
-                <Footer />
-            </div>
+            <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/team" element={<Team />} />
+                <Route path="*" element={<Home />} />
+            </Routes>
         </Router>
     );
 }
--- bp-website/src/pages/Home.tsx
+++ bp-website/src/pages/Home.tsx
@@ -1,25 +1,39 @@
 import logoAnimation from "@/assets/images/logo-loop.gif"
-import stars from "@/assets/images/awesome-parallax/stars.png"
+import stars1 from "@/assets/images/awesome-parallax/stars1.png"
+import stars2 from "@/assets/images/awesome-parallax/stars2.png"
+import stars3 from "@/assets/images/awesome-parallax/stars3.png"
 import { useRef } from "react"
 import { Parallax, ParallaxLayer, type IParallax } from '@react-spring/parallax'
+import Navbar from "@/components/Navbar"
+import Footer from "@/components/Footer";
 
 export default function Home() {
     const parallax = useRef<IParallax>(null!);
     return (
         <>
-            <Parallax ref={parallax} pages={3}>
-                <ParallaxLayer offset={3} speed={0}>
-                    <div className="grow bg-fixed bg-gradient-to-b from-void to-exosphere">
+            <Parallax ref={parallax} pages={3} className="bg-fixed bg-gradient-to-b from-void to-exosphere font-main text-2xl">
+                <ParallaxLayer offset={0} speed={0.1}>
+                    <img src={stars1} className="pixelImage w-full" />
+                </ParallaxLayer>
+                <ParallaxLayer offset={0} speed={0.15}>
+                    <img src={stars2} className="pixelImage w-full" />
+                </ParallaxLayer>
+                <ParallaxLayer offset={0} speed={0.2}>
+                    <img src={stars3} className="pixelImage w-full" />
+                </ParallaxLayer>
+                <ParallaxLayer offset={0} speed={0}>
+                    <Navbar />
+                    <div>
                         <div className="flex justify-center">
                             <img src={logoAnimation} alt="Logo Animation" className="w-150 pixelImage m-15 mb-5" />
                         </div>
-                        <div className="h-100 mx-40 mb-20 border-dark-amethyst border-10 bg-void/30 text-twilight p-5 text-3xl">
+                        <div className="h-100 mx-40 mb-20 border-dark-amethyst border-10 bg-void/50 text-twilight p-5 text-3xl">
                             Yeah yah yeah yeah yeah ye h eyah
                         </div>
                     </div>
                 </ParallaxLayer>
-                <ParallaxLayer offset={1.3} speed={-0.3}>
-                    <img src={stars} className="pixelImage w-full" />
+                <ParallaxLayer offset={2.9} speed={0}>
+                    <Footer />
                 </ParallaxLayer>
             </Parallax >
         </>
