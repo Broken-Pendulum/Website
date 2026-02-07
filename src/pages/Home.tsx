46eda392ff402645d3671dea1d750b2eced9ea17
verycoolgithubaccount <plutonium221b@gmail.com>
2026-02-07 01:08:42 -0700

    theoretically...?

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -28,7 +28,7 @@ export default function Home() {
                             <img src={logoAnimation} alt="Logo Animation" className="w-150 pixelImage m-15 mb-5" />
                         </div>
                         <div className="h-100 mx-40 mb-20 border-dark-amethyst border-10 bg-void/50 text-twilight p-5 text-3xl">
-                            Testing !!!
+                            Theoretically...?
                         </div>
                     </div>
                 </ParallaxLayer>
