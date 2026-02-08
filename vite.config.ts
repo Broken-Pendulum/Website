6c1e077322f88dae5bf0889ad0741e7f8226cf26
verycoolgithubaccount <plutonium221b@gmail.com>
2026-02-07 19:04:18 -0700

    update for domain

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,6 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vite.dev/config/
 export default defineConfig({
-    base: '/Website/',
     plugins: [react(), tailwindcss()],
     resolve: {
         alias: {
